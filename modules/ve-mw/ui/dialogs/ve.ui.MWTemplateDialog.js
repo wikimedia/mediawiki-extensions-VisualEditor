0ffd654bed3dfbac080b77d4fa9f124077788451
James D. Forrester <jforrester@wikimedia.org>
2014-01-05 20:05:05 +0800

    Bump copyright like it's 2014
    Change-Id: Ic469ebda2c061dc7da0b4c1625f43a7be55da4fa

--- modules/ve-mw/ui/dialogs/ve.ui.MWTemplateDialog.js
+++ modules/ve-mw/ui/dialogs/ve.ui.MWTemplateDialog.js
@@ -1,7 +1,7 @@
 /*
  * VisualEditor user interface MWTemplateDialog class.
  *
- * @copyright 2011-2013 VisualEditor Team and others; see AUTHORS.txt
+ * @copyright 2011-2014 VisualEditor Team and others; see AUTHORS.txt
  * @license The MIT License (MIT); see LICENSE.txt
  */
 
