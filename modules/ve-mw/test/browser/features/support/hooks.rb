0fb393823d3851b07d86837af72e4ea139c63842
cmcmahon <cmcmahon@wikimedia.org>
2014-04-28 15:32:13 -0400

    [browser test] use ?veaction=edit to avoid login where feasible
    First pass, more examination to follow. After this builds will
    continue to be green.
    
    Change-Id: I8e63046ec72ae9708d1fe708b8ee86a688ccb73c

--- modules/ve-mw/test/browser/features/support/hooks.rb
+++ modules/ve-mw/test/browser/features/support/hooks.rb
@@ -5,6 +5,15 @@ end
 #This is for the Headings test
 Before("@edit_user_page") do
   if (!$edit_user_page or !(ENV["REUSE_BROWSER"] == "true")) and @browser
+    step "I am at my user page"
+    step "I edit the page with Editing with"
+    $edit_user_page=true
+  end
+end
+
+#This is for the multiedit test
+Before("@edit_user_page_login") do
+  if (!$edit_user_page or !(ENV["REUSE_BROWSER"] == "true")) and @browser
     step "I am logged in"
     step "I am at my user page"
     step "I edit the page with Editing with"
@@ -15,9 +24,7 @@ end
 #This is for the bullets, indent-outdent, and the General Markup tests
 Before("@make_selectable_line") do
   if (!$make_selectable_line or !(ENV["REUSE_BROWSER"] == "true")) and @browser
-    step "I am logged in"
     step "I am at my user page"
-    step "I click Edit for VisualEditor"
     step "I type in an input string"
     step "select the string"
     $make_selectable_line=true
