f0f314cbbf4e7f8c4ad330a62c612a42b833b6f7
Ed Sanders <esanders@wikimedia.org>
2014-04-28 16:33:42 -0700

    Remove useless overrides in HieroInpsector
    Change-Id: Id13670bcfb4a3da112298a756415d5fe70af4b3b

--- modules/ve-mw/ui/inspectors/ve.ui.MWHieroInspector.js
+++ modules/ve-mw/ui/inspectors/ve.ui.MWHieroInspector.js
@@ -36,20 +36,6 @@ ve.ui.MWHieroInspector.static.nodeModel = ve.dm.MWHieroNode;
 
 ve.ui.MWHieroInspector.static.dir = 'ltr';
 
-/* Methods */
-
-ve.ui.MWHieroInspector.prototype.initialize = function () {
-	// Parent method
-	ve.ui.MWExtensionInspector.prototype.initialize.call( this );
-
-	this.input.$element.addClass( 've-ui-mwHieroInspector-input' );
-};
-
-ve.ui.MWHieroInspector.prototype.onOpen = function () {
-	// Parent method
-	ve.ui.MWExtensionInspector.prototype.onOpen.call( this );
-};
-
 /* Registration */
 
 ve.ui.windowFactory.register( ve.ui.MWHieroInspector );
