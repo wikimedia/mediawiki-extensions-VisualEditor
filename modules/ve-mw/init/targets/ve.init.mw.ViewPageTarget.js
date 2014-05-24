0d4aaf5b4cae033b2c306621a35326f109dece27
James D. Forrester <jforrester@wikimedia.org>
2014-05-23 17:39:04 -0700

    Move Safari 5.x to greylist because dialogs aren't working well
    Bug: 65656
    Change-Id: Idb1236d199e323381bcd69a50b34d8ba05ecc145

--- modules/ve-mw/init/targets/ve.init.mw.ViewPageTarget.js
+++ modules/ve-mw/init/targets/ve.init.mw.ViewPageTarget.js
@@ -150,7 +150,7 @@ ve.init.mw.ViewPageTarget.compatibility = {
 	'whitelist': {
 		'firefox': [['>=', 15]],
 		'iceweasel': [['>=', 10]],
-		'safari': [['>=', 5]],
+		'safari': [['>=', 6]],
 		'chrome': [['>=', 19]],
 		'opera': [['>=', 15]]
 	}
