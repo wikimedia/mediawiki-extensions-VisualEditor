e9cc4b441b0e4df271b63abb47c0edc116821e0b
Cmcmahon <cmcmahon@wikimedia.org>
2014-06-11 09:12:08 -0700

    [BrowserTest] Update login tests to use api
    This should make it possible to run all of the VE test suite
    against an arbitrary wiki (with VE installed) including
    the latest vagrant image with VisualEditor role enabled
    
    Tested on the vagrant image and on beta labs
    
    Change-Id: Ib249e4ffb607210756bb110187c7e2864b0c83f5

--- modules/ve-mw/test/browser/features/support/hooks.rb
+++ modules/ve-mw/test/browser/features/support/hooks.rb
@@ -6,7 +6,7 @@ end
 Before("@edit_user_page_login") do
   if (!$edit_user_page_login or !(ENV["REUSE_BROWSER"] == "true")) and @browser
     step "I am logged in"
-    step "I am at my user page"
+    step "I go to the browser specific edit page page"
     step "I edit the page with Editing with"
     $edit_user_page_login=true
   end
