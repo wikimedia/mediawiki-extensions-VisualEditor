06911a01caba2931ee84e8ba1edd0001069f79dc
Ed Sanders <esanders@wikimedia.org>
2014-07-28 14:23:50 +0100

    Remove old message
    Change-Id: I0e8b9ea7957f8495dc9713ac5db811f42dfb19fe

--- VisualEditorDataModule.php
+++ VisualEditorDataModule.php
@@ -76,7 +76,6 @@ class VisualEditorDataModule extends ResourceLoaderModule {
 			'summary' => array( 'summary' ),
 			'watchthis' => array( 'watchthis' ),
 			'visualeditor-browserwarning' => array( 'visualeditor-browserwarning' ),
-			'visualeditor-report-notice' => array( 'visualeditor-report-notice' ),
 			'visualeditor-wikitext-warning' => array( 'visualeditor-wikitext-warning' ),
 		);
 
