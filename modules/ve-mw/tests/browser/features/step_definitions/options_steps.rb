4aa8be0e71f076cd23da7c609101eee8b2c8a08f
ryasmeen@wikimedia.org <ryasmeen@wikimedia.org>
2014-11-04 12:21:28 +0100

    [BrowserTest] WIP adding cases to test other options under Page Settings to options.feature
    Also:
    * Fixed a typo in options_steps.rb
    * Disabled Metrics/ClassLength Rubocop cop.
    
    Change-Id: I7128ba8f7ebf9c63aef9399a9cdf24831e1a2d38

--- modules/ve-mw/tests/browser/features/step_definitions/options_steps.rb
+++ modules/ve-mw/tests/browser/features/step_definitions/options_steps.rb
@@ -1,12 +1,31 @@
-
-When(/^I click the hamburger menu$/) do
-  on(VisualEditorPage).hamburger_menu_element.when_present.click
+When(/^I click Advanced Settings$/) do
+  on(VisualEditorPage).option_advanced_settings_element.when_present.click
 end
 
 When(/^I click Options$/) do
   on(VisualEditorPage).options_in_hamburger_element.when_present.click
 end
 
+When(/^I click Page Settings$/) do
+  on(VisualEditorPage).option_page_settings_element.when_present.click
+end
+
+When(/^I click the hamburger menu$/) do
+  on(VisualEditorPage).hamburger_menu_element.when_present.click
+end
+
+When(/^I see options overlay$/) do
+  on(VisualEditorPage).options_page_title_element.when_present
+end
+
 Then(/^I should see the options overlay$/) do
-  on(VisualEditorPage).options_page_title_element.when_present.should be_visible
+  expect(on(VisualEditorPage).options_page_title_element.when_present).to be_visible
+end
+
+Then(/^the options overlay should display Advanced Settings$/) do
+  expect(on(VisualEditorPage).options_settings_content_element.text).to match "Advanced settings"
+end
+
+Then(/^the options overlay should display Page Settings$/) do
+  expect(on(VisualEditorPage).options_settings_content_element.text).to match "Page settings"
 end
