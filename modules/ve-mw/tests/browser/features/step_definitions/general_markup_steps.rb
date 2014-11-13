3ed807277eb1ede1f5eb2bf6efcdb706da30c65f
Cmcmahon <cmcmahon@wikimedia.org>
2014-11-13 09:07:41 -0700

    [BrowserTest] minor updates for hygiene, no functional changes
    Change-Id: I3d8ca33bd850caeb44b8e8e8017f2fa302676b1a

--- modules/ve-mw/tests/browser/features/step_definitions/general_markup_steps.rb
+++ modules/ve-mw/tests/browser/features/step_definitions/general_markup_steps.rb
@@ -1,16 +1,8 @@
-When(/^I click the More option$/) do
-  on(VisualEditorPage).ve_more_markup_options_element.when_present(15).click
-end
-
-When(/^I click the text style menu$/) do
-  on(VisualEditorPage).ve_text_style_element.when_present(15).click
-end
-
-And(/^I click the Bold menu option$/) do
+When(/^I click the Bold menu option$/) do
   on(VisualEditorPage).ve_bold_text_element.when_present(15).click
 end
 
-And(/^I click the Computer Code menu option$/) do
+When(/^I click the Computer Code menu option$/) do
   on(VisualEditorPage).ve_computer_code_element.when_present(15).click
 end
 
@@ -18,18 +10,26 @@ When(/^I click the Italics menu option$/) do
   on(VisualEditorPage).ve_italics_element.when_present(15).click
 end
 
-And(/^I click the Strikethrough menu option$/) do
+When(/^I click the More option$/) do
+  on(VisualEditorPage).ve_more_markup_options_element.when_present(15).click
+end
+
+When(/^I click the Strikethrough menu option$/) do
   on(VisualEditorPage).ve_strikethrough_element.when_present(15).click
 end
 
-And(/^I click the Subscript menu option$/) do
+When(/^I click the Subscript menu option$/) do
   on(VisualEditorPage).ve_subscript_element.when_present(15).click
 end
 
-And(/^I click the Superscript menu option$/) do
+When(/^I click the Superscript menu option$/) do
   on(VisualEditorPage).ve_superscript_element.when_present(15).click
 end
 
-And(/^I click the Underline menu option$/) do
+When(/^I click the text style menu$/) do
+  on(VisualEditorPage).ve_text_style_element.when_present(15).click
+end
+
+When(/^I click the Underline menu option$/) do
   on(VisualEditorPage).ve_underline_element.when_present(15).click
 end
