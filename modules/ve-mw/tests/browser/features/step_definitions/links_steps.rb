e4eeb40c17aa43854db6fb04d5ce127a233097f5
Cmcmahon <cmcmahon@wikimedia.org>
2014-11-13 10:03:49 -0700

    [BrowserTest] interim alphabetization only
    Change-Id: I3a724d2cadbc40c63978058e4b2684ef86ceec03

--- modules/ve-mw/tests/browser/features/step_definitions/links_steps.rb
+++ modules/ve-mw/tests/browser/features/step_definitions/links_steps.rb
@@ -1,8 +1,3 @@
-When(/^I click the Link button$/) do
-  sleep 1 #Chrome seems to not honor when_present correctly as of 5 Dec 2013
-  on(VisualEditorPage).ve_link_icon_element.when_present.click
-end
-
 Given(/^I can see the Link User Inteface$/) do
   on(VisualEditorPage) do |page|
     page.ve_link_ui_element.when_present
@@ -12,6 +7,32 @@ end
 
 When(/^I click Done to close Link User Interface$/) do
   on(VisualEditorPage).links_done_element.when_present.click
+end
+
+When(/^I click Links Review your changes$/) do
+  on(VisualEditorPage).review_changes_element.when_present.click
+end
+
+When(/^I click the Link button$/) do
+  sleep 1 #Chrome seems to not honor when_present correctly as of 5 Dec 2013
+  on(VisualEditorPage).ve_link_icon_element.when_present.click
+end
+
+When(/^I enter (.+) into link Content box$/) do |content|
+  on(VisualEditorPage) do |page|
+    page.link_textfield_element.when_present
+    page.link_textfield_element.send_keys(content)
+    sleep 1
+  end
+end
+
+Then(/^a non\-existing link appears in the diff view$/) do
+  on(VisualEditorPage) do |page|
+    page.wait_until(10) do
+      page.diff_view.include? "DoesNotExist"
+    end
+    page.diff_view.should match Regexp.escape("[[DoesNotExist|Links]]")
+  end
 end
 
 Then(/^an external link appears in the diff view$/) do
@@ -31,25 +52,3 @@ Then(/^an internal link appears in the diff view$/) do
     page.diff_view.should match Regexp.escape("[[Main Page|Links]]")
   end
 end
-
-Then(/^a non\-existing link appears in the diff view$/) do
-  on(VisualEditorPage) do |page|
-    page.wait_until(10) do
-      page.diff_view.include? "DoesNotExist"
-    end
-    page.diff_view.should match Regexp.escape("[[DoesNotExist|Links]]")
-  end
-end
-
-When(/^I enter (.+) into link Content box$/) do |content|
-  on(VisualEditorPage) do |page|
-    page.link_textfield_element.when_present
-    page.link_textfield_element.send_keys(content)
-    sleep 1
-  end
-end
-
-When(/^I click Links Review your changes$/) do
-  on(VisualEditorPage).review_changes_element.when_present.click
-end
-
