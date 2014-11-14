6d22cebb62573473e88afe81fe01d95a410ef153
Cmcmahon <cmcmahon@wikimedia.org>
2014-11-14 14:30:45 -0700

    [BrowserTest] remove sleep from multiedit test
    * put the shared step in shared_steps.rb
    * replace "onfocus" with simple "click"
    * update RSpec while we're at it
    
    Change-Id: I77754abdbeba1314758f5fe10601f6601e8783bd

--- modules/ve-mw/tests/browser/features/step_definitions/references_steps.rb
+++ modules/ve-mw/tests/browser/features/step_definitions/references_steps.rb
@@ -16,14 +16,6 @@ When(/^I click Insert references list$/) do
   on(VisualEditorPage).insert_references_list_element.when_present.click
 end
 
-When(/^I click Edit for VisualEditor$/) do
-  on(VisualEditorPage) do |page|
-    page.edit_ve_element.when_present.click
-    # no longer need to dismiss beta warning here https://gerrit.wikimedia.org/r/#/c/119217/
-    page.content_element.when_present(10).fire_event("onfocus")
-  end
-end
-
 When(/^I click Reference$/) do
   on(VisualEditorPage) do |page|
     page.insert_menu_element.when_present.click
