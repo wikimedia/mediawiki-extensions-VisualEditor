9600d28ebc8eacf02a5ae0bbf04f113e32947d84
Alex Monk <krenair@wikimedia.org>
2014-12-23 21:11:47 +0000

    Don't break "Edit source" link after VE has closed
    Bug: T85552
    Change-Id: I6db6fcff24110e749e3a0c1aa7c0528527b53d44

--- modules/ve-mw/init/targets/ve.init.mw.ViewPageTarget.js
+++ modules/ve-mw/init/targets/ve.init.mw.ViewPageTarget.js
@@ -1067,7 +1067,7 @@ ve.init.mw.ViewPageTarget.prototype.setupSkinTabs = function () {
 			.click( this.onViewTabClick.bind( this ) );
 
 		$( '#ca-viewsource, #ca-edit' ).click( function ( e ) {
-			if ( e.which !== 1 || e.shiftKey || e.altKey || e.ctrlKey || e.metaKey ) {
+			if ( !target.active || e.which !== 1 || e.shiftKey || e.altKey || e.ctrlKey || e.metaKey ) {
 				return;
 			}
 
