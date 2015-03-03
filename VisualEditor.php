0096c37faef1b0ab6bf0f110230437626ac85857
James D. Forrester <jforrester@wikimedia.org>
2015-03-03 13:27:37 -0800

    VisualEditor.php: Warn people to use extension.json instead
    Change-Id: I28f06581fae91d950c08c9bfc63779df6d60d4ad

--- VisualEditor.php
+++ VisualEditor.php
@@ -2,6 +2,9 @@
 /**
  * VisualEditor extension
  *
+ * This PHP entry point is deprecated. Please use wfLoadExtension() and the extension.json file
+ * instead. See https://www.mediawiki.org/wiki/Manual:Extension_registration for more details.
+ *
  * @file
  * @ingroup Extensions
  * @copyright 2011-2015 VisualEditor Team and others; see AUTHORS.txt
