160a673e7cc2d4df1553a298b094bf63c60e4b13
Ed Sanders <esanders@wikimedia.org>
2015-06-08 16:03:47 +0100

    Omit extension attributes with an undefined value
    Change-Id: I0abad51eb392644159d1845ef3a35986dc5cb79b

--- modules/ve-mw/ce/nodes/ve.ce.MWExtensionNode.js
+++ modules/ve-mw/ce/nodes/ve.ce.MWExtensionNode.js
@@ -50,14 +50,22 @@ ve.ce.MWExtensionNode.static.rendersEmpty = false;
 
 /** */
 ve.ce.MWExtensionNode.prototype.generateContents = function ( config ) {
-	var xhr,
+	var xhr, attr, wikitext,
 		deferred = $.Deferred(),
 		mwData = this.getModel().getAttribute( 'mw' ),
 		extsrc = config && config.extsrc !== undefined ? config.extsrc : mwData.body.extsrc,
 		attrs = config && config.attrs || mwData.attrs,
-		tagName = this.getModel().getExtensionName(),
-		// XML-like tags in wikitext are not actually XML and don't expect their contents to be escaped.
-		wikitext = mw.html.element( tagName, attrs, new mw.html.Raw( extsrc ) );
+		tagName = this.getModel().getExtensionName();
+
+	// undefined means omit the attribute, not convert it to string 'undefined'
+	for ( attr in mwData.attrs ) {
+		if ( mwData.attrs[attr] === undefined ) {
+			delete mwData.attrs[attr];
+		}
+	}
+
+	// XML-like tags in wikitext are not actually XML and don't expect their contents to be escaped.
+	wikitext = mw.html.element( tagName, attrs, new mw.html.Raw( extsrc ) );
 
 	if ( !this.constructor.static.rendersEmpty && extsrc.trim() !== '' ) {
 		xhr = new mw.Api().post( {
