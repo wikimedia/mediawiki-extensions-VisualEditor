6df227d4e0a093d3ef58c8c2f0e28af53de8fa2d
David Lynch <dlynch@wikimedia.org>
2015-11-04 11:46:45 -0600

    MWCommandHelpDialog: update for core changes
    Also: update VE core submodule to master (61f3fe7)
    
    New changes:
    4ef3666 Localisation updates from https://translatewiki.net.
    cfa3482 Localisation updates from https://translatewiki.net.
    d3486d4 CommandHelpDialog: Rename 'category' to 'groupName'
    b5578d1 Localisation updates from https://translatewiki.net.
    926595e Update OOjs UI to v0.13.1
    61f3fe7 [BREAKING CHANGE] Create CommandHelpRegistry for CommandHelpDialog
    
    Change-Id: If651ed30ab85175c7a262d9ccccc2e1613b0f3d6

--- modules/ve-mw/ui/dialogs/ve.ui.MWCommandHelpDialog.js
+++ modules/ve-mw/ui/dialogs/ve.ui.MWCommandHelpDialog.js
@@ -28,7 +28,6 @@ OO.inheritClass( ve.ui.MWCommandHelpDialog, ve.ui.CommandHelpDialog );
 ve.ui.MWCommandHelpDialog.static.commandGroups = ve.extendObject( ve.ui.CommandHelpDialog.static.commandGroups, {
 	insert: {
 		title: 'visualeditor-shortcuts-insert',
-		commands: {},
 		promote: [],
 		demote: []
 	}
@@ -42,32 +41,32 @@ ve.ui.windowFactory.register( ve.ui.MWCommandHelpDialog );
 	var accessKeyPrefix = $.fn.updateTooltipAccessKeys.getAccessKeyPrefix().toUpperCase().replace( /-/g, ' + ' ),
 		save = ve.msg( 'accesskey-save' );
 
-	ve.ui.MWCommandHelpDialog.static.registerCommand( 'textStyle', 'link', { sequence: [ 'wikitextLink' ] } );
-	ve.ui.MWCommandHelpDialog.static.registerCommand( 'formatting', 'blockquote', { sequence: [ 'wikitextDescription' ] } );
-	ve.ui.MWCommandHelpDialog.static.registerCommand( 'formatting', 'listNumber', { sequence: [ 'numberHash' ] } );
-	ve.ui.MWCommandHelpDialog.static.registerCommand( 'formatting', 'heading2', {
+	ve.ui.commandHelpRegistry.register( 'textStyle', 'link', { sequence: [ 'wikitextLink' ] } );
+	ve.ui.commandHelpRegistry.register( 'formatting', 'blockquote', { sequence: [ 'wikitextDescription' ] } );
+	ve.ui.commandHelpRegistry.register( 'formatting', 'listNumber', { sequence: [ 'numberHash' ] } );
+	ve.ui.commandHelpRegistry.register( 'formatting', 'heading2', {
 		sequence: [ 'wikitextHeading' ],
 		msg: 'visualeditor-formatdropdown-format-heading2'
 	} );
-	ve.ui.MWCommandHelpDialog.static.registerCommand( 'insert', 'template', {
+	ve.ui.commandHelpRegistry.register( 'insert', 'template', {
 		sequence: [ 'wikitextTemplate' ],
 		msg: 'visualeditor-dialog-template-title'
 	} );
-	ve.ui.MWCommandHelpDialog.static.registerCommand( 'insert', 'ref', {
+	ve.ui.commandHelpRegistry.register( 'insert', 'ref', {
 		sequence: [ 'wikitextRef' ],
 		msg: 'visualeditor-dialog-reference-title'
 	} );
-	ve.ui.MWCommandHelpDialog.static.registerCommand( 'insert', 'table', {
+	ve.ui.commandHelpRegistry.register( 'insert', 'table', {
 		sequence: [ 'wikitextTable' ],
 		msg: 'visualeditor-table-insert-table'
 	} );
-	ve.ui.MWCommandHelpDialog.static.registerCommand( 'insert', 'comment', {
+	ve.ui.commandHelpRegistry.register( 'insert', 'comment', {
 		sequence: [ 'wikitextComment' ],
 		msg: 'visualeditor-commentinspector-title'
 	} );
 
 	if ( save !== '-' && save !== '' ) {
-		ve.ui.MWCommandHelpDialog.static.registerCommand( 'other', 'save', {
+		ve.ui.commandHelpRegistry.register( 'other', 'save', {
 			shortcuts: [ accessKeyPrefix + save.toUpperCase() ],
 			msg: 'visualeditor-savedialog-label-save',
 			demote: true
