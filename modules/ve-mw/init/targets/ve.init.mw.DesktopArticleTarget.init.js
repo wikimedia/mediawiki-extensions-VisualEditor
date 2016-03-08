wikimedia/mediawiki-extensions-VisualEditor

commit 863a2c297415bbe60eca4c802137d180dc4aab29

Don't try to tell the server to set preferences to their current values
It's rather pointless.

Change-Id: I8065af1d61108ce843c0afd2eaa0f1f9b85b6998

modules/ve-mw/init/targets/ve.init.mw.DesktopArticleTarget.init.js
@@ -250,7 +250,7 @@
 		$.cookie( 'VEE', editor, { path: '/', expires: 30 } );
 		if ( mw.user.isAnon() ) {
 			return $.Deferred().resolve();
-		} else {
+		} else if ( mw.user.options.get( 'visualeditor-editor' ) !== editor ) {
 			return new mw.Api().saveOption( 'visualeditor-editor', editor ).then( function () {
 				mw.user.options.set( 'visualeditor-editor', editor );
 			} );

commit cf1d7c0dab98f0dbcea561ae1f66a94e97e060ad

mw.DesktopArticleTarget: Fix "Uncaught: Cannot read done of undefined"
Regression from 863a2c2974.

Sometimes when clicking "Switch to source mode" in the toolbar this
error is thrown and the interface freezes because setEditorPreference()
only returns a promise if the value is different.

> Uncaught TypeError: Cannot read property 'done' of undefined
> - ve.init.mw.DesktopArticleTarget.switchToWikitextEditor

Change-Id: Ie59f06a8ed9af9c4fb8199b013c06992d0c4f700

modules/ve-mw/init/targets/ve.init.mw.DesktopArticleTarget.init.js
@@ -258,11 +258,13 @@
 		$.cookie( 'VEE', editor, { path: '/', expires: 30 } );
 		if ( mw.user.isAnon() ) {
 			return $.Deferred().resolve();
-		} else if ( mw.user.options.get( 'visualeditor-editor' ) !== editor ) {
-			return new mw.Api().saveOption( 'visualeditor-editor', editor ).then( function () {
-				mw.user.options.set( 'visualeditor-editor', editor );
-			} );
 		}
+		if ( mw.user.options.get( 'visualeditor-editor' ) === editor ) {
+			return $.Deferred().resolve();
+		}
+		return new mw.Api().saveOption( 'visualeditor-editor', editor ).then( function () {
+			mw.user.options.set( 'visualeditor-editor', editor );
+		} );
 	}
 
 	function getLastEditor() {