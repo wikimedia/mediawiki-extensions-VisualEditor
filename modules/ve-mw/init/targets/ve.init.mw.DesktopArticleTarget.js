16e294fba71c5a9db3f5ec842550751d21c45d32
Alex Monk <alex@wikimedia.org>
2016-11-22 05:42:31 +0000

    Move around code that sets up edit notices to ensure it runs after we receive the notices to display
    Bug: T151236
    Change-Id: I63ab27cb236169f300295b2ed4c88e01048092e7

--- modules/ve-mw/init/targets/ve.init.mw.DesktopArticleTarget.js
+++ modules/ve-mw/init/targets/ve.init.mw.DesktopArticleTarget.js
@@ -1414,6 +1414,7 @@ ve.init.mw.DesktopArticleTarget.prototype.maybeShowMetaDialog = function () {
 	if ( this.welcomeDialogPromise ) {
 		this.welcomeDialogPromise
 			.always( function () {
+				var noticesTool;
 				// Pop out the notices when the welcome dialog is closed
 				if (
 					target.switched &&
@@ -1421,7 +1422,9 @@ ve.init.mw.DesktopArticleTarget.prototype.maybeShowMetaDialog = function () {
 				) {
 					target.actionsToolbar.tools.editModeSource.getPopup().toggle( true );
 				} else {
-					target.actionsToolbar.tools.notices.getPopup().toggle( true );
+					noticesTool = target.actionsToolbar.tools.notices;
+					noticesTool.setNotices( target.getEditNotices() );
+					noticesTool.getPopup().toggle( true );
 				}
 			} );
 	}
