8079d081d038e20cfdac8cde2931e319e9ba5c26
Fomafix <fomafix@googlemail.com>
2018-02-12 10:46:01 +0100

    Remove /* global confirm, alert */
    The browser functions alert() and confirm() are not used.
    
    Change-Id: I1b069937fe1548298d55a3e6616e189b72c3e8fa

--- modules/ve-mw/init/targets/ve.init.mw.DesktopArticleTarget.js
+++ modules/ve-mw/init/targets/ve.init.mw.DesktopArticleTarget.js
@@ -5,8 +5,6 @@
  * @license The MIT License (MIT); see LICENSE.txt
  */
 
-/* global confirm, alert */
-
 /**
  * MediaWiki desktop article target.
  *
--- modules/ve-mw/init/ve.init.mw.ArticleTarget.js
+++ modules/ve-mw/init/ve.init.mw.ArticleTarget.js
@@ -5,7 +5,7 @@
  * @license The MIT License (MIT); see LICENSE.txt
  */
 
-/* global EasyDeflate, alert */
+/* global EasyDeflate */
 
 /**
  * Initialization MediaWiki article target.
