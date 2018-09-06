e050bb9ecf0f9d4b67088c2406e5d0b16b1e6a31
David Lynch <dlynch@wikimedia.org>
2018-09-06 11:45:52 -0500

    dm.MWTableNode: include paragraph in suggestedParentNodeTypes
    This allows cleanup of empty paragraphs following block-level insertions to
    work.
    
    Bug: T201573
    Change-Id: Id537d86c72c01ab2c71d2043e906f4c32ef755eb

--- modules/ve-mw/dm/nodes/ve.dm.MWTableNode.js
+++ modules/ve-mw/dm/nodes/ve.dm.MWTableNode.js
@@ -42,7 +42,14 @@ ve.dm.MWTableNode.static.classAttributes = {
 };
 
 // Tables in wikitext only work in some contexts, they're impossible e.g. in list items
-ve.dm.MWTableNode.static.suggestedParentNodeTypes = [ 'document', 'div', 'tableCell', 'tableCaption', 'mwImageCaption' ];
+ve.dm.MWTableNode.static.suggestedParentNodeTypes = [
+	'document', 'div', 'tableCell', 'tableCaption', 'mwImageCaption',
+	// TODO: `paragraph` isn't really a suggested table parent. However,
+	// allowing it here interacts with our post-insertion cleanup for block
+	// nodes so that empty paragraphs get properly removed. We should find a
+	// cleaner way to do this. See: T201573.
+	'paragraph'
+];
 
 // HACK: users of parentNodeTypes should be fixed to check for inherited classes.
 ve.dm.TableSectionNode.static.parentNodeTypes.push( 'mwTable' );
