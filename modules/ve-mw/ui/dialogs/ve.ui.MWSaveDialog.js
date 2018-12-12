f2ac575ee2c8d1e08da2f912fe49af4fddde380e
Ed Sanders <esanders@wikimedia.org>
2018-12-12 12:53:55 +0000

    Move save error messages above save footer
    Keeps the more important information higher up the page.
    
    Bug: T72266
    Change-Id: Iaf29a5190af5f72c88eb1bfdacce199cd046eee4

--- modules/ve-mw/ui/dialogs/ve.ui.MWSaveDialog.js
+++ modules/ve-mw/ui/dialogs/ve.ui.MWSaveDialog.js
@@ -641,8 +641,8 @@ ve.ui.MWSaveDialog.prototype.initialize = function () {
 		this.$editSummaryLabel,
 		this.editSummaryInput.$element,
 		this.$saveOptions,
-		this.$saveFoot,
-		this.$saveMessages
+		this.$saveMessages,
+		this.$saveFoot
 	);
 
 	// Review panel
