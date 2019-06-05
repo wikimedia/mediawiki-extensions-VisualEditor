f48b5bcc9a12df0a5979d6de60f514e0f096295a
Bartosz Dziewoński <matma.rex@gmail.com>
2019-06-05 10:27:32 -0400

    MobileArticleTarget: Fix silly indirection via a global variable
    Change-Id: I71349313b493c18cebe30df0b1b03defe81bb80e

--- modules/ve-mw/init/targets/ve.init.mw.MobileArticleTarget.js
+++ modules/ve-mw/init/targets/ve.init.mw.MobileArticleTarget.js
@@ -504,7 +504,7 @@ ve.init.mw.MobileArticleTarget.prototype.setupToolbar = function ( surface ) {
 	if ( !this.$title ) {
 		this.$title = $( '<div>' ).addClass( 've-init-mw-mobileArticleTarget-title-container' ).append(
 			$( '<div>' ).addClass( 've-init-mw-mobileArticleTarget-title' ).text(
-				new mw.Title( ve.init.target.getPageName() ).getMainText()
+				new mw.Title( this.getPageName() ).getMainText()
 			)
 		);
 	}
