0371915832d3974d87bf18f77f571b16ee270dcd
Ed Sanders <esanders@wikimedia.org>
2019-09-10 14:15:01 +0100

    Add unit tests for read-mode reference filter
    Bug: T150418
    Change-Id: I8b39694d5c01e0902491fe3d6f3a08451c7c5297

--- modules/ve-mw/tests/ce/ve.ce.Surface.test.js
+++ modules/ve-mw/tests/ce/ve.ce.Surface.test.js
@@ -86,6 +86,22 @@ QUnit.test( 'beforePaste/afterPaste', function ( assert ) {
 				expectedRangeOrSelection: new ve.Range( 5 ),
 				expectedHtml: '<p><span typeof="mw:Entity">-</span><span typeof="mw:Entity" id="mw-reference-cite">-</span></p>',
 				msg: 'RESTBase IDs still stripped if used when important attributes dropped'
+			},
+			{
+				documentHtml: '<p></p>',
+				rangeOrSelection: new ve.Range( 1 ),
+				pasteHtml: 'a<sup id="cite_ref-1" class="reference"><a href="./Article#cite_note-1">[1]</a></sup>b',
+				expectedRangeOrSelection: new ve.Range( 3 ),
+				expectedHtml: '<p>ab</p>',
+				msg: 'Read mode references stripped'
+			},
+			{
+				documentHtml: '<p></p>',
+				rangeOrSelection: new ve.Range( 1 ),
+				pasteHtml: 'a<sup typeof="mw:Extension/ref" data-mw="{}" class="mw-ref" about="#mwt1" id="cite_ref-foo-0" rel="dc:references"><a href="./Article#cite_note-foo-0"><span class="mw-reflink-text ve-pasteProtect">[1]</span></a></sup>b',
+				expectedRangeOrSelection: new ve.Range( 5 ),
+				expectedHtml: '<p>a<sup typeof="mw:Extension/ref" data-mw="{&quot;name&quot;:&quot;ref&quot;}" class="mw-ref"><a style="counter-reset: mw-Ref 1;"><span class="mw-reflink-text">[1]</span></a></sup>b</p>',
+				msg: 'VE references not stripped'
 			}
 		];
 
