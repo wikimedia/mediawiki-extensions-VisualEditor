wikimedia/mediawiki-extensions-VisualEditor

commit c66f8e0547cb9f436eefaaa9293ffcfd06faae89

MWInternalLinkAnnotation: handle non-internal links that say they're mw:WikiLink
The likely case for this is: copying from within VE in one wiki, and pasting
into VE in another wiki. This change will notice this happening, and fall back
to treat it as an external link. (For the wiki-internal links, this will turn
them into interwiki links rather than raw external links.)

Bug: T223322
Change-Id: Ie0157fc3aee6e5fd9973a2889be7ebd287bc90a5

modules/ve-mw/dm/annotations/ve.dm.MWInternalLinkAnnotation.js
@@ -45,6 +45,10 @@ ve.dm.MWInternalLinkAnnotation.static.toDataElement = function ( domElements, co
 		);
 	}
 
+	if ( !targetData.isInternal ) {
+		return ve.dm.MWExternalLinkAnnotation.static.toDataElement( domElements, converter );
+	}
+
 	return {
 		type: this.name,
 		attributes: {

commit e472a4bbd34f745d2e2b1a5cea69a3230f91ad9e

Fix mw:MediaLink support
* Handle mw:MediaLinks pointing to to non-existent files, which come
  with typeof="mw:Error" (similar to image nodes).

* Fix regression from c66f8e0547cb9f436eefaaa9293ffcfd06faae89, which
  caused all mw:MediaLinks to be treated as plain external links again.

* Add test cases.

Bug: T232754
Change-Id: I9ae5bcfc4e24e8c0d22ef77d6a4d03f817fc9768

modules/ve-mw/dm/annotations/ve.dm.MWInternalLinkAnnotation.js
@@ -32,6 +32,9 @@ ve.dm.MWInternalLinkAnnotation.static.name = 'link/mwInternal';
 
 ve.dm.MWInternalLinkAnnotation.static.matchRdfaTypes = [ 'mw:WikiLink', 'mw:MediaLink' ];
 
+// mw:MediaLink to non-existent files come with typeof="mw:Error"
+ve.dm.MWInternalLinkAnnotation.static.allowedRdfaTypes = [ 'mw:Error' ];
+
 ve.dm.MWInternalLinkAnnotation.static.toDataElement = function ( domElements, converter ) {
 	var targetData,
 		resource = domElements[ 0 ].getAttribute( 'resource' );
@@ -43,10 +46,10 @@ ve.dm.MWInternalLinkAnnotation.static.toDataElement = function ( domElements, co
 			domElements[ 0 ].getAttribute( 'href' ),
 			converter.getTargetHtmlDocument()
 		);
-	}
 
-	if ( !targetData.isInternal ) {
-		return ve.dm.MWExternalLinkAnnotation.static.toDataElement( domElements, converter );
+		if ( !targetData.isInternal ) {
+			return ve.dm.MWExternalLinkAnnotation.static.toDataElement( domElements, converter );
+		}
 	}
 
 	return {

modules/ve-mw/tests/dm/ve.dm.mwExample.js
@@ -248,6 +248,26 @@ ve.dm.mwExample.MWInternalSectionLink.absoluteData = {
 	}
 };
 
+ve.dm.mwExample.MWMediaLinkExistsData = {
+	type: 'link/mwInternal',
+	attributes: {
+		lookupTitle: 'Media:Exists.png',
+		normalizedTitle: 'Media:Exists.png',
+		origTitle: 'Media:Exists.png',
+		title: 'Media:Exists.png'
+	}
+};
+
+ve.dm.mwExample.MWMediaLinkMissingData = {
+	type: 'link/mwInternal',
+	attributes: {
+		lookupTitle: 'Media:Missing.png',
+		normalizedTitle: 'Media:Missing.png',
+		origTitle: 'Media:Missing.png',
+		title: 'Media:Missing.png'
+	}
+};
+
 ve.dm.mwExample.MWBlockImage = {
 	html:
 		'<figure typeof="mw:Image/Thumb" class="mw-halign-right foobar">' +
@@ -1494,6 +1514,61 @@ ve.dm.mwExample.domToDataCases = {
 			{ type: 'internalList' },
 			{ type: '/internalList' }
 		]
+	},
+	'mw:MediaLink (exists)': {
+		body: '<p><a rel="mw:MediaLink" href="//localhost/w/images/x/xx/Exists.png" resource="./Media:Exists.png" title="Exists.png">Media:Exists.png</a></p>',
+		data: [
+			{ type: 'paragraph' },
+			[ 'M', [ ve.dm.mwExample.MWMediaLinkExistsData ] ],
+			[ 'e', [ ve.dm.mwExample.MWMediaLinkExistsData ] ],
+			[ 'd', [ ve.dm.mwExample.MWMediaLinkExistsData ] ],
+			[ 'i', [ ve.dm.mwExample.MWMediaLinkExistsData ] ],
+			[ 'a', [ ve.dm.mwExample.MWMediaLinkExistsData ] ],
+			[ ':', [ ve.dm.mwExample.MWMediaLinkExistsData ] ],
+			[ 'E', [ ve.dm.mwExample.MWMediaLinkExistsData ] ],
+			[ 'x', [ ve.dm.mwExample.MWMediaLinkExistsData ] ],
+			[ 'i', [ ve.dm.mwExample.MWMediaLinkExistsData ] ],
+			[ 's', [ ve.dm.mwExample.MWMediaLinkExistsData ] ],
+			[ 't', [ ve.dm.mwExample.MWMediaLinkExistsData ] ],
+			[ 's', [ ve.dm.mwExample.MWMediaLinkExistsData ] ],
+			[ '.', [ ve.dm.mwExample.MWMediaLinkExistsData ] ],
+			[ 'p', [ ve.dm.mwExample.MWMediaLinkExistsData ] ],
+			[ 'n', [ ve.dm.mwExample.MWMediaLinkExistsData ] ],
+			[ 'g', [ ve.dm.mwExample.MWMediaLinkExistsData ] ],
+			{ type: '/paragraph' },
+			{ type: 'internalList' },
+			{ type: '/internalList' }
+		],
+		normalizedBody: '<p><a href="./Media:Exists.png" rel="mw:WikiLink" resource="./Media:Exists.png" title="Exists.png">Media:Exists.png</a></p>',
+		fromDataBody: '<p><a href="./Media:Exists.png" rel="mw:WikiLink">Media:Exists.png</a></p>'
+	},
+	'mw:MediaLink (missing)': {
+		body: '<p><a rel="mw:MediaLink" href="./Special:FilePath/Missing.png" resource="./Media:Missing.png" title="Missing.png" typeof="mw:Error" data-mw=\'{"errors":[{"key":"apierror-filedoesnotexist","message":"This image does not exist."}]}\'>Media:Missing.png</a></p>',
+		data: [
+			{ type: 'paragraph' },
+			[ 'M', [ ve.dm.mwExample.MWMediaLinkMissingData ] ],
+			[ 'e', [ ve.dm.mwExample.MWMediaLinkMissingData ] ],
+			[ 'd', [ ve.dm.mwExample.MWMediaLinkMissingData ] ],
+			[ 'i', [ ve.dm.mwExample.MWMediaLinkMissingData ] ],
+			[ 'a', [ ve.dm.mwExample.MWMediaLinkMissingData ] ],
+			[ ':', [ ve.dm.mwExample.MWMediaLinkMissingData ] ],
+			[ 'M', [ ve.dm.mwExample.MWMediaLinkMissingData ] ],
+			[ 'i', [ ve.dm.mwExample.MWMediaLinkMissingData ] ],
+			[ 's', [ ve.dm.mwExample.MWMediaLinkMissingData ] ],
+			[ 's', [ ve.dm.mwExample.MWMediaLinkMissingData ] ],
+			[ 'i', [ ve.dm.mwExample.MWMediaLinkMissingData ] ],
+			[ 'n', [ ve.dm.mwExample.MWMediaLinkMissingData ] ],
+			[ 'g', [ ve.dm.mwExample.MWMediaLinkMissingData ] ],
+			[ '.', [ ve.dm.mwExample.MWMediaLinkMissingData ] ],
+			[ 'p', [ ve.dm.mwExample.MWMediaLinkMissingData ] ],
+			[ 'n', [ ve.dm.mwExample.MWMediaLinkMissingData ] ],
+			[ 'g', [ ve.dm.mwExample.MWMediaLinkMissingData ] ],
+			{ type: '/paragraph' },
+			{ type: 'internalList' },
+			{ type: '/internalList' }
+		],
+		normalizedBody: '<p><a href="./Media:Missing.png" rel="mw:WikiLink" resource="./Media:Missing.png" title="Missing.png" typeof="mw:Error" data-mw=\'{"errors":[{"key":"apierror-filedoesnotexist","message":"This image does not exist."}]}\'>Media:Missing.png</a></p>',
+		fromDataBody: '<p><a href="./Media:Missing.png" rel="mw:WikiLink">Media:Missing.png</a></p>'
 	},
 	'numbered external link (empty mw:Extlink)': {
 		body: '<p>Foo<a rel="mw:ExtLink" href="http://www.example.com"></a>Bar</p>',