wikimedia/mediawiki-extensions-VisualEditor

commit 4545f53245260e5b1ecf05364cd2b127d207121e

build: Update eslint-config-wikimedia to 0.17.0
Fix instances of variable shadowing.

Change-Id: I6e2befb020d7d4b506c7b46131eafacd951aa6d1

modules/ve-mw/preinit/ve.init.mw.ArticleTargetSaver.js
@@ -53,10 +53,9 @@
 			var i, len;
 
 			function copyAttributes( from, to ) {
-				var i, len;
-				for ( i = 0, len = from.attributes.length; i < len; i++ ) {
-					to.setAttribute( from.attributes[ i ].name, from.attributes[ i ].value );
-				}
+				from.attributes.forEach( function ( attr ) {
+					to.setAttribute( attr.name, attr.value );
+				} );
 			}
 
 			if ( oldDoc ) {
@@ -238,7 +237,7 @@
 			return request.then(
 				function ( response, jqxhr ) {
 					var eventData, fullEventName, error,
-						data = response[ action ];
+						responseData = response[ action ];
 
 					// Log data about the request if eventName was set
 					if ( options.track && options.eventName ) {
@@ -247,25 +246,25 @@
 							duration: options.now() - start
 						};
 						fullEventName = 'performance.system.' + options.eventName +
-							( data.cachekey ? '.withCacheKey' : '.withoutCacheKey' );
+							( responseData.cachekey ? '.withCacheKey' : '.withoutCacheKey' );
 						options.track( fullEventName, eventData );
 					}
 
-					if ( !data ) {
+					if ( !responseData ) {
 						error = {
 							code: 'invalidresponse',
 							html: mw.message( 'api-clientside-error-invalidresponse' ).parse()
 						};
-					} else if ( data.result !== 'success' ) {
+					} else if ( responseData.result !== 'success' ) {
 						// This should only happen when saving an edit and getting a captcha from ConfirmEdit
 						// extension (`data.result === 'error'`). It's a silly special case...
 						return $.Deferred().reject( 'no-error-no-success', response ).promise();
 					} else {
 						// paction specific errors
-						switch ( data.paction ) {
+						switch ( responseData.paction ) {
 							case 'save':
 							case 'serialize':
-								if ( typeof data.content !== 'string' ) {
+								if ( typeof responseData.content !== 'string' ) {
 									error = {
 										code: 'invalidcontent',
 										html: mw.message( 'api-clientside-error-invalidresponse' ).parse()
@@ -273,7 +272,7 @@
 								}
 								break;
 							case 'diff':
-								if ( typeof data.diff !== 'string' ) {
+								if ( typeof responseData.diff !== 'string' ) {
 									error = {
 										code: 'invalidcontent',
 										html: mw.message( 'api-clientside-error-invalidresponse' ).parse()
@@ -287,7 +286,7 @@
 						// Use the same format as API errors
 						return $.Deferred().reject( error.code, { errors: [ error ] } ).promise();
 					}
-					return data;
+					return responseData;
 				},
 				function ( code, response ) {
 					var eventData, fullEventName,

Gruntfile.js
@@ -195,8 +195,8 @@ module.exports = function ( grunt ) {
 				grunt.log.error( 'Unstaged changes in these files:' );
 				grunt.log.error( ret );
 				// Show a condensed diff
-				require( 'child_process' ).exec( 'git diff -U1 | tail -n +3', function ( err, stdout, stderr ) {
-					grunt.log.write( err || stderr || stdout );
+				require( 'child_process' ).exec( 'git diff -U1 | tail -n +3', function ( err2, stdout2, stderr2 ) {
+					grunt.log.write( err2 || stderr2 || stdout2 );
 					done( false );
 				} );
 			} else {

build/screenshots-client/utils.js
@@ -1,5 +1,5 @@
 module.exports = function () {
-	var done = arguments[ arguments.length - 1 ];
+	var veDone = arguments[ arguments.length - 1 ];
 
 	window.seleniumUtils = {
 		getBoundingRect: function ( elements ) {
@@ -114,7 +114,7 @@ module.exports = function () {
 			target.actionsToolbar.tools.notices.getPopup().toggle( false );
 			// Wait for save button fade
 			setTimeout( function () {
-				done( { width: window.innerWidth, height: window.innerHeight } );
+				veDone( { width: window.innerWidth, height: window.innerHeight } );
 			}, 100 );
 		} );
 	} );

modules/ve-mw-collab/ve.init.mw.CollabTarget.init.js
@@ -289,7 +289,7 @@
 		} );
 	}
 
-	function importTitle() {
+	function onImportSubmit() {
 		importInput.getValidity().then( function () {
 			var title = mw.Title.newFromText( importInput.getValue().trim() );
 
@@ -306,8 +306,8 @@
 		return !!mw.Title.newFromText( value );
 	} );
 	importInput.on( 'change', onImportChange );
-	importInput.on( 'enter', importTitle );
-	importButton.on( 'click', importTitle );
+	importInput.on( 'enter', onImportSubmit );
+	importButton.on( 'click', onImportSubmit );
 	onImportChange();
 
 	if ( pageTitle ) {

modules/ve-mw/ce/nodes/ve.ce.MWSignatureNode.js
@@ -127,9 +127,9 @@ ve.ce.MWSignatureNode.prototype.generateContents = function () {
 		onlypst: true
 	} )
 		.done( function ( resp ) {
-			var wikitext = ve.getProp( resp, 'parse', 'text' );
-			if ( wikitext ) {
-				ve.init.target.parseWikitextFragment( wikitext, true, doc ).then( function ( response ) {
+			var wt = ve.getProp( resp, 'parse', 'text' );
+			if ( wt ) {
+				ve.init.target.parseWikitextFragment( wt, true, doc ).then( function ( response ) {
 					if ( ve.getProp( response, 'visualeditor', 'result' ) !== 'success' ) {
 						deferred.reject();
 						return;

modules/ve-mw/dm/nodes/ve.dm.MWTransclusionNode.js
@@ -235,13 +235,13 @@ ve.dm.MWTransclusionNode.static.describeChanges = function ( attributeChanges ) 
 	// the same, so it doesn't have to consider whether the actual template
 	// involved has changed.
 
-	function getLabel( param ) {
+	function getLabel( par ) {
 		// If a parameter is an object with a wt key, we just want the value of that.
-		if ( param && param.wt !== undefined ) {
+		if ( par && par.wt !== undefined ) {
 			// Can be `''`, and we're okay with that
-			return param.wt;
+			return par.wt;
 		}
-		return param;
+		return par;
 	}
 
 	if ( attributeChanges.mw.from.parts.length === 1 && attributeChanges.mw.to.parts.length === 1 ) {

modules/ve-mw/init/targets/ve.init.mw.DesktopArticleTarget.js
@@ -265,13 +265,13 @@ ve.init.mw.DesktopArticleTarget.prototype.setupToolbar = function ( surface ) {
 		}
 
 		this.toolbarSetupDeferred.done( function () {
-			var surface = target.getSurface();
+			var newSurface = target.getSurface();
 			// Check the surface wasn't torn down while the toolbar was animating
-			if ( surface ) {
+			if ( newSurface ) {
 				ve.track( 'trace.initializeToolbar.enter', { mode: mode } );
 				target.getToolbar().initialize();
-				surface.getView().emit( 'position' );
-				surface.getContext().updateDimensions();
+				newSurface.getView().emit( 'position' );
+				newSurface.getContext().updateDimensions();
 				ve.track( 'trace.initializeToolbar.exit', { mode: mode } );
 				ve.track( 'trace.activate.exit', { mode: mode } );
 			}

modules/ve-mw/preinit/ve.init.MWEditingTabDialog.js
@@ -72,19 +72,18 @@ mw.libs.ve.EditingTabDialog.prototype.getSetupProcess = function ( action ) {
  * @inheritdoc
  */
 mw.libs.ve.EditingTabDialog.prototype.getActionProcess = function ( action ) {
-	var dialog = this,
-		act = action;
-	if ( act ) {
+	var dialog = this;
+	if ( action ) {
 		return new OO.ui.Process( function () {
-			var action = this.getActions().get( { actions: act } )[ 0 ];
-			action.pushPending();
+			var actionWidget = this.getActions().get( { actions: action } )[ 0 ];
+			actionWidget.pushPending();
 			dialog.pushPending();
 
 			// Same as ve.init.target.getLocalApi()
-			new mw.Api().saveOption( 'visualeditor-tabs', act ).done( function () {
-				action.popPending();
-				mw.user.options.set( 'visualeditor-tabs', act );
-				dialog.close( { action: act } );
+			new mw.Api().saveOption( 'visualeditor-tabs', action ).done( function () {
+				actionWidget.popPending();
+				mw.user.options.set( 'visualeditor-tabs', action );
+				dialog.close( { action: action } );
 			} );
 		}, this );
 	} else {

commit 879f006b7b52571f1c99ce1d29b79689adbecf7d

Fix regression when opening save dialog
DOM attributes are not arrays, and so don't have Array#forEach.

Fixed regression introduced in 4545f53245260e5b1ecf05364cd2b127d207121e.

Change-Id: I1f0f44747a0f8a376c1fb7cbb8862c096a9d1dc9

modules/ve-mw/preinit/ve.init.mw.ArticleTargetSaver.js
@@ -53,9 +53,10 @@
 			var i, len;
 
 			function copyAttributes( from, to ) {
-				from.attributes.forEach( function ( attr ) {
-					to.setAttribute( attr.name, attr.value );
-				} );
+				var i2, len2;
+				for ( i2 = 0, len2 = from.attributes.length; i2 < len2; i2++ ) {
+					to.setAttribute( from.attributes[ i2 ].name, from.attributes[ i2 ].value );
+				}
 			}
 
 			if ( oldDoc ) {