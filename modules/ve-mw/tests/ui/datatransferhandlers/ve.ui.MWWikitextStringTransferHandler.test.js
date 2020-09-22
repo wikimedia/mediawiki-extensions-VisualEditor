2a7f31b5b971e34f32c93bc011df9b7b6abb79bb
James D. Forrester <jforrester@wikimedia.org>
2020-09-22 12:26:00 +0100

    Update VE core submodule to master (95f9f5681)
    New changes:
    cb4613044 FindAndReplace: Always highlight results when opening
    cf480dbb6 ElementLinearData: Remap annotations on nodes too when sanitizing pastes
    705743230 ElementLinearData: Remap annotations on moved meta nodes too
    16be2c262 ElementLinearData: Remove moved metadata too when removing metadata
    6d51882e2 ElementLinearData: Deduplicate annotations when sanitizing pastes
    
    Bug: T191487
    Bug: T259730
    Bug: T262877
    Change-Id: I0918ad9833c15998b1696ec40c1681c0d8f14236

--- modules/ve-mw/tests/ui/datatransferhandlers/ve.ui.MWWikitextStringTransferHandler.test.js
+++ modules/ve-mw/tests/ui/datatransferhandlers/ve.ui.MWWikitextStringTransferHandler.test.js
@@ -188,10 +188,10 @@ QUnit.test( 'convert', function ( assert ) {
 				annotations: [],
 				assertDom: true,
 				expectedData: [
-					{ type: 'mwHeading', attributes: { level: 2 }, internal: { changesSinceLoad: 0, metaItems: [] }, originalDomElements: $( '<h2>foo</h2>' ).toArray() },
+					{ type: 'mwHeading', attributes: { level: 2 }, internal: { changesSinceLoad: 0 }, originalDomElements: $( '<h2>foo</h2>' ).toArray() },
 					'f', 'o', 'o',
 					{ type: '/mwHeading' },
-					{ type: 'mwHeading', attributes: { level: 2 }, internal: { changesSinceLoad: 0, metaItems: [] }, originalDomElements: $( '<h2 id="mw-meaningful-id">bar</h2>' ).toArray() },
+					{ type: 'mwHeading', attributes: { level: 2 }, internal: { changesSinceLoad: 0 }, originalDomElements: $( '<h2 id="mw-meaningful-id">bar</h2>' ).toArray() },
 					'b', 'a', 'r',
 					{ type: '/mwHeading' },
 					{ type: 'internalList' },
@@ -206,7 +206,7 @@ QUnit.test( 'convert', function ( assert ) {
 				annotations: [],
 				assertDom: true,
 				expectedData: [
-					{ type: 'mwHeading', attributes: { level: 2 }, internal: { changesSinceLoad: 0, metaItems: [] }, originalDomElements: $( '<h2 id="Tudnivalók"> Tudnivalók </h2>' ).toArray() },
+					{ type: 'mwHeading', attributes: { level: 2 }, internal: { changesSinceLoad: 0 }, originalDomElements: $( '<h2 id="Tudnivalók"> Tudnivalók </h2>' ).toArray() },
 					'T', 'u', 'd', 'n', 'i', 'v', 'a', 'l', 'ó', 'k',
 					{ type: '/mwHeading' },
 					{ type: 'internalList' },
