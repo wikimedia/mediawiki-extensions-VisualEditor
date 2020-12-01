fb0e33f987c5f5c715677014c18715bb601ed687
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2020-12-01 08:43:33 +0100

    Fix incomplete return in DesktopArticleTarget.tryTeardown()
    If a teardown has started, there should be a teardownPromise,
    otherwise return a rejected promise.
    
    Bug: T268358
    Change-Id: Ia5cbd6b409a38f97243234ea7c87d24f71bdf3d6

--- modules/ve-mw/init/targets/ve.init.mw.DesktopArticleTarget.js
+++ modules/ve-mw/init/targets/ve.init.mw.DesktopArticleTarget.js
@@ -559,7 +559,7 @@ ve.init.mw.DesktopArticleTarget.prototype.teardownNewSection = function ( surfac
  */
 ve.init.mw.DesktopArticleTarget.prototype.tryTeardown = function ( noPrompt, trackMechanism ) {
 	if ( this.deactivating || ( !this.active && !this.activating ) ) {
-		return;
+		return this.teardownPromise || ve.createDeferred().reject().promise();
 	}
 
 	// Just in case these weren't closed before
