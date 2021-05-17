85f45fd49df8700e8ce1c3b0618522e6fb7947df
Adam Wight <adam.wight@wikimedia.de>
2021-05-17 12:25:46 +0200

    Introduce feature flag for template dialog sidebar changes
    Bug: T274263
    Change-Id: If274efa94474c3de79c5917b76c1a290f93172e1

--- includes/VisualEditorHooks.php
+++ includes/VisualEditorHooks.php
@@ -1050,6 +1050,7 @@ class VisualEditorHooks {
 			'transclusionDialogSuggestedValues' => $veConfig->get( 'VisualEditorTransclusionDialogSuggestedValues' ),
 			'transclusionDialogInlineDescriptions' =>
 				$veConfig->get( 'VisualEditorTransclusionDialogInlineDescriptions' ),
+			'transclusionDialogNewSidebar' => $veConfig->get( 'VisualEditorTransclusionDialogNewSidebar' ),
 			'cirrusSearchLookup' => $extensionRegistry->isLoaded( 'CirrusSearch' )
 				&& $veConfig->get( 'VisualEditorTemplateSearchImprovements' ),
 			'templateSearchImprovements' => $veConfig->get( 'VisualEditorTemplateSearchImprovements' ),
