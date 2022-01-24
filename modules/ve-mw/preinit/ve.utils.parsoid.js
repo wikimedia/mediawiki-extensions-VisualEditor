a2390efbf46d010de14e24ebd6bf9e3de05615d2
Bartosz Dziewoński <matma.rex@gmail.com>
2022-01-24 22:11:29 +0100

    Re-apply "Re-duplicate deduplicated TemplateStyles"
    Previous attempt in 005a8d24efa1c72dd4676e41e57e77c91c82a498,
    reverted in 3c1d167b3380a01b16e9e7fc8577e45ca708b331.
    
    The deduplicateStyles() function lacked a check for fosterable
    positions, which caused T299767. This is now fixed.
    
    Also added tests.
    
    Bug: T287675
    Bug: T299251
    Change-Id: I0d22be9b66d26d09373cee63dd6ce52c1659e62d

--- modules/ve-mw/preinit/ve.utils.parsoid.js
+++ modules/ve-mw/preinit/ve.utils.parsoid.js
@@ -94,6 +94,110 @@ mw.libs.ve.stripRestbaseIds = function ( doc ) {
 };
 
 /**
+ * Re-duplicate deduplicated TemplateStyles, for correct rendering when editing a section or
+ * when templates are removed during the edit.
+ *
+ * @param {HTMLElement} element Parent element, e.g. document body
+ */
+mw.libs.ve.reduplicateStyles = function ( element ) {
+	Array.prototype.forEach.call( element.querySelectorAll( 'link[rel="mw-deduplicated-inline-style"]' ), function ( link ) {
+		var href = link.getAttribute( 'href' );
+		if ( !href || href.slice( 0, 'mw-data:'.length ) !== 'mw-data:' ) {
+			return;
+		}
+		var key = href.slice( 'mw-data:'.length );
+		var style = element.querySelector( 'style[data-mw-deduplicate="' + key + '"]' );
+		if ( !style ) {
+			return;
+		}
+
+		var newStyle = link.ownerDocument.createElement( 'style' );
+		newStyle.setAttribute( 'data-mw-deduplicate', key );
+
+		// Copy content from the old `style` node (for rendering)
+		for ( var i = 0; i < style.childNodes.length; i++ ) {
+			newStyle.appendChild( style.childNodes[ i ].cloneNode( true ) );
+		}
+		// Copy attributes from the old `link` node (for selser)
+		Array.prototype.forEach.call( link.attributes, function ( attr ) {
+			if ( attr.name !== 'rel' && attr.name !== 'href' ) {
+				newStyle.setAttribute( attr.name, attr.value );
+			}
+		} );
+
+		link.parentNode.replaceChild( newStyle, link );
+	} );
+
+	Array.prototype.forEach.call( element.querySelectorAll( 'style[data-mw-deduplicate]:empty' ), function ( style ) {
+		var key = style.getAttribute( 'data-mw-deduplicate' );
+		var firstStyle = element.querySelector( 'style[data-mw-deduplicate="' + key + '"]' );
+		if ( !firstStyle || firstStyle === style ) {
+			return;
+		}
+
+		// Copy content from the first matching `style` node (for rendering)
+		for ( var i = 0; i < firstStyle.childNodes.length; i++ ) {
+			style.appendChild( firstStyle.childNodes[ i ].cloneNode( true ) );
+		}
+	} );
+};
+
+/**
+ * De-duplicate TemplateStyles, like Parsoid does.
+ *
+ * @param {HTMLElement} element Parent element, e.g. document body
+ */
+mw.libs.ve.deduplicateStyles = function ( element ) {
+	/**
+	 * Check whether `node` is in a fosterable position. (Nodes in these positions may be moved
+	 * elsewhere in the DOM by the HTML5 parsing algorithm, if they don't have the right tag name.)
+	 * https://html.spec.whatwg.org/#appropriate-place-for-inserting-a-node
+	 *
+	 * @private
+	 * @param {Node|null} node
+	 * @return {boolean}
+	 */
+	function isFosterablePosition( node ) {
+		var fosterablePositions = [ 'table', 'thead', 'tbody', 'tfoot', 'tr' ];
+		return node && fosterablePositions.indexOf( node.parentNode.nodeName.toLowerCase() ) !== -1;
+	}
+
+	var styleTagKeys = {};
+
+	Array.prototype.forEach.call( element.querySelectorAll( 'style[data-mw-deduplicate]' ), function ( style ) {
+		var key = style.getAttribute( 'data-mw-deduplicate' );
+
+		if ( !styleTagKeys[ key ] ) {
+			// Not a dupe
+			styleTagKeys[ key ] = true;
+			return;
+		}
+
+		if ( !isFosterablePosition( style ) ) {
+			// Dupe - replace with a placeholder <link> reference
+			var link = style.ownerDocument.createElement( 'link' );
+			link.setAttribute( 'rel', 'mw-deduplicated-inline-style' );
+			link.setAttribute( 'href', 'mw-data:' + key );
+
+			// Copy attributes from the old `link` node (for selser)
+			Array.prototype.forEach.call( style.attributes, function ( attr ) {
+				if ( attr.name !== 'rel' && attr.name !== 'data-mw-deduplicate' ) {
+					link.setAttribute( attr.name, attr.value );
+				}
+			} );
+
+			style.parentNode.replaceChild( link, style );
+		} else {
+			// Duplicate style tag found in fosterable position.
+			// Not deduping it (to avoid corruption when the resulting HTML is parsed: T299767),
+			// but emptying out the style tag for consistency with Parsoid.
+			// Parsoid says it does this for performance reasons.
+			style.innerHTML = '';
+		}
+	} );
+};
+
+/**
  * Fix fragment links which should be relative to the current document
  *
  * This prevents these links from trying to navigate to another page,
