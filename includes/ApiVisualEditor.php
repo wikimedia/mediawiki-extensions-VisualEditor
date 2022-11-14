4bdb4917f060922dae9d046fcf645e5dda723953
csimiyu <csimiyu@wikimedia.org>
2022-11-14 15:03:27 +0300

    Remove usage of User::isBlockedGlobally and User::getGlobalBlock
    Bug: T318898
    Change-Id: I727cda9008866b665395cd0e9969c920dc9c28d6

--- includes/ApiVisualEditor.php
+++ includes/ApiVisualEditor.php
@@ -577,9 +577,7 @@ class ApiVisualEditor extends ApiBase {
 				$block = null;
 				$blockinfo = null;
 				// Blocked user notice
-				if ( $user->isBlockedGlobally() ) {
-					$block = $user->getGlobalBlock();
-				} elseif ( $permissionManager->isBlockedFrom( $user, $title, true ) ) {
+				if ( $permissionManager->isBlockedFrom( $user, $title, true ) ) {
 					$block = $user->getBlock();
 				}
 				if ( $block ) {
