5f8dbcf2dd7c86c8989ec33018e7a96c7d0c2c88
Esther Akinloose <estherakinloose@gmail.com>
2022-11-30 13:43:32 +0000

    selenium: Toolbar should open notices popup as soon as it loads
    Bug: T296187
    Change-Id: I705d4d3e8a0135b500ec63749f519f7b86855a2f

--- tests/selenium/specs/toolbar.js
+++ tests/selenium/specs/toolbar.js
@@ -1,7 +1,6 @@
 'use strict';
 const assert = require( 'assert' );
 const EditPage = require( '../pageobjects/edit.page' );
-const LoginPage = require( 'wdio-mediawiki/LoginPage' );
 const Util = require( 'wdio-mediawiki/Util' );
 
 describe( 'Toolbar', function () {
@@ -10,8 +9,6 @@ describe( 'Toolbar', function () {
 
 	beforeEach( async function () {
 		name = Util.getTestString();
-		await LoginPage.loginAdmin();
-
 		await EditPage.openForEditing( name );
 		await EditPage.toolbar.waitForDisplayed( { timeout: 20000 } );
 	} );
@@ -21,6 +18,10 @@ describe( 'Toolbar', function () {
 		// 'Leave site? Changes that you made may not be saved. Cancel/Leave'
 		// appears after the browser tries to leave the page with the preview.
 		await browser.reloadSession();
+	} );
+
+	it( 'should open notices popup as soon as it loads', async function () {
+		assert( await EditPage.notices.isDisplayed() );
 	} );
 
 	it( 'should open switch editor', async function () {
