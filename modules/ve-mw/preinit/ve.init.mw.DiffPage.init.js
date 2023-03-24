7dbe38577346768ec6b90c203dc6127eb15982a3
Sam Wilson <sam@samwilson.id.au>
2023-03-24 14:12:34 +0800

    Move the diff-mode selector to the new DifferenceEngineBeforeDiffTable hook
    Use the new hook to add the diff-mode selector to the area directly before
    the diff table.
    
    Also toggles the new inline-diff legend, when the initial diff-type is 'inline'.
    
    Depends-On: I6de30bf79eb5ac262285951792782b870d075e00
    Bug: T324759
    Change-Id: Ifc133856dd793693c3a2722a7b1319dfe74555a2

--- modules/ve-mw/preinit/ve.init.mw.DiffPage.init.js
+++ modules/ve-mw/preinit/ve.init.mw.DiffPage.init.js
@@ -15,6 +15,7 @@
 		progress = new OO.ui.ProgressBarWidget( { classes: [ 've-init-mw-diffPage-loading' ] } ),
 		originalUrl = new URL( location.href ),
 		initMode = originalUrl.searchParams.get( 'diffmode' ) || mw.user.options.get( 'visualeditor-diffmode-historical' ) || 'source',
+		initType = originalUrl.searchParams.get( 'diff-type' ),
 		conf = mw.config.get( 'wgVisualEditorConfig' ),
 		pluginModules = conf.pluginModules.filter( mw.loader.getState );
 
@@ -45,6 +46,11 @@
 		$wikitextDiffBody.toggleClass( 'oo-ui-element-hidden', isVisual );
 		var $revSlider = $( '.mw-revslider-container' );
 		$revSlider.toggleClass( 've-init-mw-diffPage-revSlider-visual', isVisual );
+		if ( initType === 'inline' ) {
+			$( 'body' )
+				.toggleClass( 'mw-diff-type-not-inline', isVisual )
+				.toggleClass( 'mw-diff-type-inline', !isVisual );
+		}
 		if ( isVisual ) {
 			// Highlight the headers using the same styles as the diff, to better indicate
 			// the meaning of headers when not using two-column diff.
