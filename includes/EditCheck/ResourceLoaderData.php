70c116a17a8a3fefd9b8f7314a9a1fcb8d75f544
Ed Sanders <esanders@wikimedia.org>
2023-10-03 16:20:51 +0100

    Edit check: Simplify "experience" config to "maximumEditcount"
    There's no product need for anything other than an upper limit
    on edit count. If one arises in the future we can adjust accordingly,
    but better to keep the JSON, and any UI implementation, simple
    for now.
    
    Change-Id: I892847ad78b19695f0f0f664002d3c566f7806de

--- includes/EditCheck/ResourceLoaderData.php
+++ includes/EditCheck/ResourceLoaderData.php
@@ -19,8 +19,7 @@ class ResourceLoaderData {
 			// TODO: when we have multiple edit checks this will likely be a generic block:
 			// account: loggedin, loggedout, anything non-truthy means allow either
 			'account' => false,
-			// experience: [ comparison, number ], compared to wgUserEditCount
-			'experience' => [ '<', 100 ],
+			'maximumEditcount' => 100
 		],
 	];
 
