6cbedbf9d230fa68260d6fb3fea0354a32244439
Timo Tijhof <krinkle@fastmail.com>
2024-05-01 18:25:33 +0100

    tests: Adopt QUnit.test.each() and native async-await in a few places
    * Remove need for manual hacking of sub groups via "msg" strings
      carefully prepended to every assertion.
    
    * Improve CI details, by reporting the specific case that failed,
      and local dev via ability to re-run each case, and reporting names
      directly in the HTML Reporter and CLI summary.
    
    * Reduce need for assert.async() and tracking of callbacks, especially
      to improve failure details in case of Promise rejection.
      Current logic was likely to cause a confusing timeout instead of a
      clear failure if the promise ends up rejected.
    
      QUnit propagates these as part of awaiting and asserting the test
      closure's promise value (as async fn) automatically.
    
      This approach also avoids the pitfal of a falsely passing test
      when an assertion inside a done() handler was never reached.
    
    * Use modern for-of where possible to remove need for closures and
      arrow functions. Thus reducing complexity of test code, where
      complexity should be kept lowest to avoid false confidence.
    
    * Use plain for-in instead of overly complex Object.keys().forEach().
    
    Change-Id: I934a266e75e64371081f104cfb867fb2c282c84a

--- modules/ve-mw/tests/ui/datatransferhandlers/ve.ui.UrlStringTransferHandler.test.js
+++ modules/ve-mw/tests/ui/datatransferhandlers/ve.ui.UrlStringTransferHandler.test.js
@@ -8,31 +8,27 @@ QUnit.module( 've.ui.UrlStringTransferHandler (MW)' );
 
 /* Tests */
 
-QUnit.test( 'paste', ( assert ) => {
-	const cases = [
-			{
-				msg: 'External link converts to internal link',
-				pasteString: location.origin + mw.Title.newFromText( 'Main Page' ).getUrl(),
-				pasteType: 'text/plain',
-				expectedData: () => {
-					// Explicitly create an internal link so we can assert this behaviour is working
-					const a = ve.dm.MWInternalLinkAnnotation.static.newFromTitle( mw.Title.newFromText( 'Main Page' ) ).element;
-					return [
-						[ 'M', [ a ] ],
-						[ 'a', [ a ] ],
-						[ 'i', [ a ] ],
-						[ 'n', [ a ] ],
-						[ ' ', [ a ] ],
-						[ 'P', [ a ] ],
-						[ 'a', [ a ] ],
-						[ 'g', [ a ] ],
-						[ 'e', [ a ] ]
-					];
-				}
-			}
-		];
-
-	cases.forEach( ( caseItem ) => {
-		ve.test.utils.runUrlStringHandlerTest( assert, caseItem.pasteString, caseItem.pasteHtml, caseItem.pasteType, caseItem.expectedData, location.origin, caseItem.msg );
-	} );
+QUnit.test.each( 'paste', [
+	{
+		msg: 'External link converts to internal link',
+		pasteString: location.origin + mw.Title.newFromText( 'Main Page' ).getUrl(),
+		pasteType: 'text/plain',
+		expectedData: () => {
+			// Explicitly create an internal link so we can assert this behaviour is working
+			const a = ve.dm.MWInternalLinkAnnotation.static.newFromTitle( mw.Title.newFromText( 'Main Page' ) ).element;
+			return [
+				[ 'M', [ a ] ],
+				[ 'a', [ a ] ],
+				[ 'i', [ a ] ],
+				[ 'n', [ a ] ],
+				[ ' ', [ a ] ],
+				[ 'P', [ a ] ],
+				[ 'a', [ a ] ],
+				[ 'g', [ a ] ],
+				[ 'e', [ a ] ]
+			];
+		}
+	}
+], ( assert, caseItem ) => {
+	ve.test.utils.runUrlStringHandlerTest( assert, caseItem.pasteString, caseItem.pasteHtml, caseItem.pasteType, caseItem.expectedData, location.origin, caseItem.msg );
 } );
