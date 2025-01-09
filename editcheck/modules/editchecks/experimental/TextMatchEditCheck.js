a6217877e8f5e09311a47c6cb35d483eca01685b
David Lynch <dlynch@wikimedia.org>
2025-01-09 12:05:57 -0600

    Enhance the TextMatch check into an actual check
    * Now restricted to only modified ranges
    * Lets replacers be defined by on-wiki config
    * Lets replacers choose to extend the fragment beyond the matched text
    * Lets replacers choose which listener to act within
    
    Change-Id: I76d18911ff4d44de4514ca36aa94545e7094d3cb

--- editcheck/modules/editchecks/experimental/TextMatchEditCheck.js
+++ editcheck/modules/editchecks/experimental/TextMatchEditCheck.js
@@ -1,16 +1,29 @@
 mw.editcheck.TextMatchEditCheck = function MWTextMatchEditCheck( /* config */ ) {
 	// Parent constructor
 	mw.editcheck.TextMatchEditCheck.super.apply( this, arguments );
+
+	this.replacers = [
+		...mw.editcheck.TextMatchEditCheck.static.replacers,
+		...( this.config.replacers || [] )
+	];
 };
 
 OO.inheritClass( mw.editcheck.TextMatchEditCheck, mw.editcheck.BaseEditCheck );
 
 mw.editcheck.TextMatchEditCheck.static.name = 'textMatch';
 
-mw.editcheck.TextMatchEditCheck.static.choices = [];
+mw.editcheck.TextMatchEditCheck.static.choices = [
+	{
+		action: 'delete',
+		label: ve.msg( 'visualeditor-contextitemwidget-label-remove' )
+	},
+	{
+		action: 'dismiss',
+		label: ve.msg( 'ooui-dialog-process-dismiss' )
+	}
+];
 
 mw.editcheck.TextMatchEditCheck.static.replacers = [
-	// TODO: Load text replacement rules from community config
 	{
 		query: 'unfortunately',
 		title: 'Adverb usage',
@@ -18,24 +31,65 @@ mw.editcheck.TextMatchEditCheck.static.replacers = [
 	}
 ];
 
-mw.editcheck.TextMatchEditCheck.prototype.onDocumentChange = function ( surfaceModel ) {
+mw.editcheck.TextMatchEditCheck.prototype.handleListener = function ( surfaceModel, listener ) {
 	const actions = [];
-	this.constructor.static.replacers.forEach( ( replacer ) => {
-		surfaceModel.getDocument().findText( replacer.query ).forEach( ( range ) => {
-			const fragment = surfaceModel.getLinearFragment( range );
-			actions.push(
-				new mw.editcheck.EditCheckAction( {
-					fragments: [ fragment ],
-					title: replacer.title,
-					message: replacer.message,
-					check: this
-				} )
-			);
-		} );
+	const modified = this.getModifiedContentRanges( surfaceModel.getDocument() );
+	this.replacers.forEach( ( replacer ) => {
+		if ( replacer.listener && replacer.listener !== listener ) {
+			return;
+		}
+		surfaceModel.getDocument().findText( replacer.query )
+			.filter( ( range ) => !this.isDismissedRange( range ) )
+			.filter( ( range ) => modified.some( ( modRange ) => range.touchesRange( modRange ) ) )
+			.filter( ( range ) => this.isRangeInValidSection( range, surfaceModel.documentModel ) )
+			.forEach( ( range ) => {
+				let fragment = surfaceModel.getLinearFragment( range );
+				switch ( replacer.expand ) {
+					case 'sentence':
+						// TODO: implement once unicodejs support is added
+						break;
+					case 'paragraph':
+						fragment = fragment.expandLinearSelection( 'closest', ve.dm.ContentBranchNode )
+							// …but that covered the entire CBN, we only want the contents
+							.adjustLinearSelection( 1, -1 );
+						break;
+					case 'word':
+					case 'siblings':
+					case 'parent':
+						fragment = fragment.expandLinearSelection( replacer.expand );
+						break;
+				}
+				actions.push(
+					new mw.editcheck.EditCheckAction( {
+						fragments: [ fragment ],
+						title: replacer.title,
+						message: replacer.message,
+						check: this
+					} )
+				);
+			} );
 	} );
 	return actions;
 };
 
-// mw.editcheck.TextMatchEditCheck.prototype.onBeforeSave = mw.editcheck.TextMatchEditCheck.prototype.onDocumentChange;
+mw.editcheck.TextMatchEditCheck.prototype.onDocumentChange = function ( surfaceModel ) {
+	return this.handleListener( surfaceModel, 'onDocumentChange' );
+};
+
+mw.editcheck.TextMatchEditCheck.prototype.onBeforeSave = function ( surfaceModel ) {
+	return this.handleListener( surfaceModel, 'onBeforeSave' );
+};
+
+mw.editcheck.TextMatchEditCheck.prototype.act = function ( choice, action /* , surface */ ) {
+	switch ( choice ) {
+		case 'dismiss':
+			this.dismiss( action );
+			break;
+		case 'delete':
+			action.fragments[ 0 ].removeContent();
+			break;
+	}
+	return ve.createDeferred().resolve( {} );
+};
 
 mw.editcheck.editCheckFactory.register( mw.editcheck.TextMatchEditCheck );
