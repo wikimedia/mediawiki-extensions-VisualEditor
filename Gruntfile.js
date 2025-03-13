a63cd3700c756c1f5b06f66a8aacad1530d44d4e
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2025-03-13 13:47:09 +0000

    build: Updating @babel/runtime to 7.26.10
    * https://github.com/advisories/GHSA-968p-4wvh-cqc8
    
    Additional changes:
    * Enable stylelint caching.
    
    Change-Id: I6403218b9e9df1ad4e3c45cb97f83f31a0e2b061

--- Gruntfile.js
+++ Gruntfile.js
@@ -120,7 +120,8 @@ module.exports = function ( grunt ) {
 		},
 		stylelint: {
 			options: {
-				reportNeedlessDisables: true
+				reportNeedlessDisables: true,
+				cache: true
 			},
 			all: [
 				'**/*.{css,less}',
