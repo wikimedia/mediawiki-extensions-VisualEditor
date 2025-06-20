2ac3d4a559c29e7436bb70deccc175b30ab242e3
Ed Sanders <esanders@wikimedia.org>
2025-06-20 12:28:44 +0100

    EditCheck: Use separate files for separate classes
    Change-Id: Icfc4c4373b9f6fd0f953cf3cb83810918dbdf4b6

---
+++ editcheck/modules/EditCheckDialogTool.js
@@ -0,0 +1,20 @@
+/**
+ * @class
+ * @extends ve.ui.ToolbarDialogTool
+ * @constructor
+ * @param {OO.ui.ToolGroup} toolGroup
+ * @param {Object} [config] Configuration options
+ */
+ve.ui.EditCheckDialogTool = function VeUiEditCheckDialogTool() {
+	ve.ui.EditCheckDialogTool.super.apply( this, arguments );
+};
+OO.inheritClass( ve.ui.EditCheckDialogTool, ve.ui.ToolbarDialogTool );
+ve.ui.EditCheckDialogTool.static.name = 'editCheckDialog';
+ve.ui.EditCheckDialogTool.static.group = 'notices';
+ve.ui.EditCheckDialogTool.static.icon = 'robot';
+ve.ui.EditCheckDialogTool.static.title = 'Edit check'; // OO.ui.deferMsg( 'visualeditor-dialog-command-help-title' );
+ve.ui.EditCheckDialogTool.static.autoAddToCatchall = false;
+ve.ui.EditCheckDialogTool.static.commandName = 'editCheckDialogInProcessToggle';
+
+// Demo button for opening edit check sidebar
+ve.ui.toolFactory.register( ve.ui.EditCheckDialogTool );
--- editcheck/modules/init.js
+++ editcheck/modules/init.js
@@ -13,9 +13,13 @@ mw.editcheck = {
 
 require( './utils.js' );
 require( './EditCheckDialog.js' );
+require( './FixedEditCheckDialog.js' );
+require( './SidebarEditCheckDialog.js' );
 require( './EditCheckGutterSidebarDialog.js' );
+require( './EditCheckPreSaveToolbarTools.js' );
 require( './EditCheckFactory.js' );
 require( './EditCheckAction.js' );
+require( './EditCheckActionWidget.js' );
 require( './BaseEditCheck.js' );
 require( './AsyncTextCheck.js' );
 
@@ -98,44 +102,3 @@ if ( mw.config.get( 'wgVisualEditorConfig' ).editCheck || mw.editcheck.forceEnab
 		controller.setup();
 	} );
 }
-
-// This is for the toolbar:
-
-ve.ui.EditCheckBack = function VeUiEditCheckBack() {
-	// Parent constructor
-	ve.ui.EditCheckBack.super.apply( this, arguments );
-
-	this.setDisabled( false );
-};
-OO.inheritClass( ve.ui.EditCheckBack, ve.ui.Tool );
-ve.ui.EditCheckBack.static.name = 'editCheckBack';
-ve.ui.EditCheckBack.static.icon = 'previous';
-ve.ui.EditCheckBack.static.autoAddToCatchall = false;
-ve.ui.EditCheckBack.static.autoAddToGroup = false;
-ve.ui.EditCheckBack.static.title =
-	OO.ui.deferMsg( 'visualeditor-backbutton-tooltip' );
-ve.ui.EditCheckBack.prototype.onSelect = function () {
-	const surface = this.toolbar.getSurface();
-	surface.getContext().hide();
-	surface.execute( 'window', 'close', 'fixedEditCheckDialog' );
-	this.setActive( false );
-	ve.track( 'activity.' + this.getName(), { action: 'tool-used' } );
-};
-ve.ui.EditCheckBack.prototype.onUpdateState = function () {
-	this.setDisabled( false );
-};
-ve.ui.toolFactory.register( ve.ui.EditCheckBack );
-
-ve.ui.EditCheckSaveDisabled = function VeUiEditCheckSaveDisabled() {
-	// Parent constructor
-	ve.ui.EditCheckSaveDisabled.super.apply( this, arguments );
-};
-OO.inheritClass( ve.ui.EditCheckSaveDisabled, ve.ui.MWSaveTool );
-ve.ui.EditCheckSaveDisabled.static.name = 'showSaveDisabled';
-ve.ui.EditCheckSaveDisabled.static.autoAddToCatchall = false;
-ve.ui.EditCheckSaveDisabled.static.autoAddToGroup = false;
-ve.ui.EditCheckSaveDisabled.prototype.onUpdateState = function () {
-	this.setDisabled( true );
-};
-
-ve.ui.toolFactory.register( ve.ui.EditCheckSaveDisabled );
