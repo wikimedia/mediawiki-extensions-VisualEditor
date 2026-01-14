a9e0e90d43ff2461856ee32810f70edebf0d8ecd
David Lynch <dlynch@wikimedia.org>
2026-01-13 18:39:32 -0600

    ConvertReferenceEditCheck: introduce strictness configuration
    `strict` config is introduced, can be:
    * 'url-only': the reference must contain only a single URL
    * 'covers': the reference must be entirely covered by a single link
    * false: anything the context item triggers for counts
    
    Defaults to url-only, as the most restrictive.
    
    This should avoid us throwing away information that was part of the
    reference contents, as without this we'll offer to convert any
    non-template reference that contains an external link.
    
    Bug: T413279
    Change-Id: I4d0c65ecd68b22cbf50291644b4cd45769f03de8

--- editcheck/modules/editchecks/experimental/ConvertReferenceEditCheck.js
+++ editcheck/modules/editchecks/experimental/ConvertReferenceEditCheck.js
@@ -5,6 +5,14 @@ mw.editcheck.ConvertReferenceEditCheck = function MWConvertReferenceEditCheck() 
 
 OO.inheritClass( mw.editcheck.ConvertReferenceEditCheck, mw.editcheck.BaseEditCheck );
 
+mw.editcheck.ConvertReferenceEditCheck.static.defaultConfig = ve.extendObject( {}, mw.editcheck.BaseEditCheck.static.defaultConfig, {
+	// strict can be, in descending order of strictness:
+	// * 'url-only': the reference must entirely consist of only a single URL
+	// * 'covers': the reference must be entirely covered by a single link annotation
+	// * anything-else: anything that the context item triggers for counts
+	strict: 'url-only'
+} );
+
 mw.editcheck.ConvertReferenceEditCheck.static.title = OO.ui.deferMsg( 'citoid-referencecontextitem-convert-button' );
 
 mw.editcheck.ConvertReferenceEditCheck.static.description = OO.ui.deferMsg( 'citoid-referencecontextitem-convert-message' );
@@ -34,10 +42,45 @@ mw.editcheck.ConvertReferenceEditCheck.prototype.onDocumentChange = function ( s
 		const referenceNode = node.getInternalItem();
 		const href = ve.ui.CitoidReferenceContextItem.static.getConvertibleHref( referenceNode );
 		if ( href ) {
-			return node.getOuterRange();
-		} else {
-			return null;
+			switch ( this.config.strict ) {
+				case 'url-only': {
+					// Restrict this further than the context item does: require that the contents
+					// of the reference be a single URL.
+					// i.e. <ref>https://example.com</ref>
+					const referenceRange = referenceNode.getRange();
+					const protocols = ve.init.platform.getUnanchoredExternalLinkUrlProtocolsRegExp().source;
+					const urlPattern = new RegExp( protocols + '\\S+', 'ig' );
+					const text = documentModel.data.getText( false, referenceRange );
+					if ( urlPattern.test( text ) ) {
+						return node.getOuterRange();
+					}
+					// Also catch <ref>[https:///example.com]</ref>
+					// This turns into `{ref}{paragraph}{mwnumberedexternallink}{/}{/}{/}`
+					if ( referenceRange.getLength() === 4 ) {
+						const contentNode = ve.getProp( referenceNode, 'children', 0, 'children', 0 );
+						if ( contentNode instanceof ve.dm.MWNumberedExternalLinkNode ) {
+							return node.getOuterRange();
+						}
+					}
+					break;
+				}
+				case 'covered': {
+					// Restrict this further than the context item does: require that the link annotation
+					// completely covers the contents of the referenceNode. This should mean that either
+					// it's a linked URL, or it's just the title of the external page that has been linked,
+					// and so we won't potentially be throwing away data like pages, publication names, etc.
+					const annotations = referenceNode.getAnnotationRanges();
+					if ( annotations.length === 1 && ( annotations[ 0 ].range.getLength() + 2 === referenceNode.getRange().getLength() ) ) {
+						return node.getOuterRange();
+					}
+					break;
+				}
+				default:
+					// The context item was enough
+					return node.getOuterRange();
+			}
 		}
+		return null;
 	} ).filter( ( obj ) => obj ).filter( ( range ) => !this.isDismissedRange( range ) ).map( ( range ) => (
 		new mw.editcheck.EditCheckAction( {
 			fragments: [ surfaceModel.getLinearFragment( range ) ],
