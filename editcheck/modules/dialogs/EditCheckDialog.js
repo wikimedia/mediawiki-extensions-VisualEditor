a14113324b342637a79dfe520081d7ffe53f818a
Caro Medelius <cmedelius@wikimedia.org>
2026-03-17 11:06:16 -0700

    EditCheck: fix behavior of the onAct listener that we add to actions
    Makes sure that the correct listener is removed before adding it again.
    
    Change-Id: I8e525bf16f74197e4fa5c63b8649eb56df53d2a6

--- editcheck/modules/dialogs/EditCheckDialog.js
+++ editcheck/modules/dialogs/EditCheckDialog.js
@@ -188,7 +188,7 @@ ve.ui.EditCheckDialog.prototype.refresh = function () {
 	this.currentActions.forEach( ( action, index ) => {
 		const widget = action.render( action !== this.currentAction, this.singleAction, this.surface );
 		widget.on( 'togglecollapse', this.onToggleCollapse, [ action, index ], this );
-		action.off( 'act', this.onAct ).on( 'act', this.onAct, [ action, widget ], this );
+		action.off( 'act', this.onAct, this ).on( 'act', this.onAct, [ action, widget ], this );
 
 		this.$actions.append( widget.$element );
 	} );
