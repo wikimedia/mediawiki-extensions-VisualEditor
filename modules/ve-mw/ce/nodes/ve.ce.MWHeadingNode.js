4b4ac548badabbbb353fa3ba6ef7ad0c8ce2201c
Ed Sanders <esanders@wikimedia.org>
2026-04-15 16:16:58 +0100

    MWHeadingNode: Add mw-heading* classes
    Bug: T423437
    Change-Id: I62eaa3938a112896a6c4fb63d83c2968b0b49b8e

--- modules/ve-mw/ce/nodes/ve.ce.MWHeadingNode.js
+++ modules/ve-mw/ce/nodes/ve.ce.MWHeadingNode.js
@@ -17,6 +17,8 @@
 ve.ce.MWHeadingNode = function VeCeMWHeadingNode() {
 	// Parent constructor
 	ve.ce.MWHeadingNode.super.apply( this, arguments );
+
+	this.updateClasses();
 };
 
 /* Inheritance */
@@ -26,6 +28,35 @@ OO.inheritClass( ve.ce.MWHeadingNode, ve.ce.HeadingNode );
 /* Static Properties */
 
 ve.ce.MWHeadingNode.static.name = 'mwHeading';
+
+/* Methods */
+
+ve.ce.MWHeadingNode.prototype.onUpdate = function () {
+	// Parent method
+	ve.ce.MWHeadingNode.super.prototype.onUpdate.call( this );
+
+	this.updateClasses();
+};
+
+/**
+ * Update CSS classes form model state
+ */
+ve.ce.MWHeadingNode.prototype.updateClasses = function () {
+	// Add the section wrapper class to the heading itself
+	// so that the correct padding is applied.
+	// This currently only affects Minerva
+	// eslint-disable-next-line mediawiki/class-doc
+	this.$element
+		.removeClass( [
+			'mw-heading1',
+			'mw-heading2',
+			'mw-heading3',
+			'mw-heading4',
+			'mw-heading5',
+			'mw-heading6'
+		] )
+		.addClass( 'mw-heading mw-heading' + this.model.getAttribute( 'level' ) );
+};
 
 /* Registration */
 
